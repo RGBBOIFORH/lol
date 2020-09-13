d8783fcf10c68c45e17fed3e51eec5870109be7f
RGBBOIFORH <70192413+RGBBOIFORH@users.noreply.github.com>
2020-09-13 17:02:49 +0200

    Create whitelist.php

---
+++ whitelist.php
@@ -0,0 +1,117 @@
+<?php
+    function rString($n) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+
+        for ($i = 0; $i < $n; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+
+        return $randomString;
+    }
+    $Key = $_GET['key'];
+
+    // This method is really bad, use arrays.
+    if ($Key == 'urdadverygae') {
+        echo ($Key);
+    } elseif ($Key == 'GODLYMANXD') {
+        echo ($Key);
+    } elseif ($Key == 'AISSBeeter412') {
+        echo ($Key);
+    } elseif ($Key == 'SYNAPSE_CHEAT') {
+        echo ($Key);
+    } elseif ($Key == 'ComInMehDade') {
+        echo ($Key);
+    } elseif ($Key == 'neutronsycth') {
+        echo ($Key);
+    } elseif ($Key == 'JBMoneyAlt3') {
+        echo ($Key);
+    } elseif ($Key == 'JBMoneyAlt2') {
+        echo ($Key);
+    } elseif ($Key == 'JBMoneyAlt1') {
+        echo ($Key);
+    } elseif ($Key == 'AIbert_son232') {
+        echo ($Key);
+    } elseif ($Key == 'PhantomsmurfZ') {
+        echo ($Key);
+    } elseif ($Key == '90xnosadhehe') {
+        echo ($Key);
+    } elseif ($Key == 'nishthedish1') {
+        echo ($Key);
+    } elseif ($Key == 'donkhang') {
+        echo ($Key);
+    } elseif ($Key == 'besthussain1') {
+        echo ($Key);
+    } elseif ($Key == 'mrsawsomcooI') {
+        echo ($Key);
+    } elseif ($Key == 'BsiweI20') {
+        echo ($Key);
+    } elseif ($Key == 'lLoveJaiIbreak_123') {
+        echo ($Key);
+    } elseif ($Key == 'lolerz780') {
+        echo ($Key);
+    } elseif ($Key == 'IlllIIIlllIIIIlllI') {
+        echo ($Key);
+    } elseif ($Key == 'AlfiethePROatGames') {
+        echo ($Key);  
+    } elseif ($Key == 'Sniperbank') {
+        echo ($Key); 
+    } elseif ($Key == 'ImTrance') {
+        echo ($Key); 
+    } elseif ($Key == 'TylerTheGrea8') {
+        echo ($Key); 
+    } elseif ($Key == 'overnamed23') {
+        echo ($Key); 
+    } elseif ($Key == 'Naryxian') {
+        echo ($Key);
+    } elseif ($Key == 'LemonMilkLord') {
+        echo ($Key);
+    } elseif ($Key == 'IsHa4aN') {
+        echo ($Key);
+    } elseif ($Key == 'ItzLightningBoltz') {
+        echo ($Key);
+    } elseif ($Key == 'Imbackumad2') {
+        echo ($Key);
+    } elseif ($Key == 'Imbackumad1') {
+        echo ($Key);
+    } elseif ($Key == 'BaconTheMean') {
+        echo ($Key);
+    } elseif ($Key == '96_o') {
+        echo ($Key);
+    } elseif ($Key == 'deathkillerb3ast') {
+        echo ($Key);
+    } elseif ($Key == 'MotrixHalvator') {
+        echo ($Key);
+    } elseif ($Key == 'nennwnwwnbe') {
+        echo ($Key);
+    } elseif ($Key == 'iOmqiAlex') {
+        echo ($Key);
+    } elseif ($Key == 'alrobloxeano') {
+        echo ($Key);
+    } elseif ($Key == 'TIDYINMYMOUTH421') {
+        echo ($Key);
+    } elseif ($Key == 'ilikemangoAlot') {
+        echo ($Key);
+    } elseif ($Key == 'SidewaysStyIe') {
+        echo ($Key);
+    } elseif ($Key == 'k00laidkid1x1') {
+        echo ($Key);
+    } elseif ($Key == 'IIDevFrostII') {
+        echo ($Key);
+    } elseif ($Key == 'qtdjw7ja1naldkq1') {
+        echo ($Key);
+    } elseif ($Key == 'linsonder06') {
+        echo ($Key);
+    } elseif ($Key == 'jb36hdgamer') {
+        echo ($Key);
+    } elseif ($Key == 'WprkcIock') {
+        echo ($Key);
+    } elseif ($Key == 'YoHiEveryone123') {
+        echo ($Key);
+    } elseif ($Key == 'SwagManJimbo') {
+        echo ($Key);
+    } else {
+        echo rString(rand(1, 999));
+    }
+?>
